393e385080240ee33c91f0ca632383fe89a561a6
Abdullah Darsot <93137191+AbdullahDarsot@users.noreply.github.com>
2021-11-23 21:28:32 +0000

    Add files via upload

--- Challenge/src/F1.java
+++ Challenge/src/F1.java
@@ -1,21 +1,28 @@
 
 public class F1 {
-//	private String favouriteTeam = "RedBull";
+	private String favouriteTeam = "RedBull";
 	private String Track = "Singapore";
 	private int numberOfCars = 2;
 //	private int pitCrew = 20;
+	private int RedBullPoints = 25;
+	private int MercedesPoints = 22;
 	private int pointsToWin = 25;
+//	private int Position;
 	
 	public void ShowFavouriteTrack() {
-		String ShowFavouriteTrack = String.format("The name of my favourite Track is %s", Track);
+		String ShowFavouriteTrack = String.format("The name of my favourite Track is %s and my favourite Team is %s", Track, favouriteTeam);
 		System.out.println(ShowFavouriteTrack);
-//		System.out.println("The name of the car is " + name);
-//		System.out.println("The model of the car is " + model);
 	}
 	
 	public void increasePoints() {
 		pointsToWin +=25;
 		System.out.println("Total points after winning " + pointsToWin);
+		
+		if (RedBullPoints > MercedesPoints) {
+			System.out.println("Red Bull are first");
+		}else {
+			System.out.println("Mercedes are first");
+		}
 	}
 	
 	public void numberOfcars() {
