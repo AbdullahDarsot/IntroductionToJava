677c206f3ac552700135423e893d6611bdfd02f6
Abdullah Darsot <93137191+AbdullahDarsot@users.noreply.github.com>
2021-11-29 09:03:45 +0000

    Add files via upload

--- TopTrumpsChallenge/src/Cars.java
+++ TopTrumpsChallenge/src/Cars.java
@@ -2,65 +2,118 @@
 public class Cars {
 	String make;
 	String model;
-	int wheels;
-	boolean engineOn;
+	float engineSize;
+	int topSpeed;
+	int horsePower;
+	float accelaration;
+	int price;
 	
 	
-	public Cars(String Make, String Model, int Wheels) {
+	public Cars(String Make, String Model, float EngineSize, int TopSpeed, int HorsePower, float Accelaration, int Price) {
 		make = Make;
 		model = Model;
-		wheels = Wheels;
-		
-	
-	}
-	
-	void startEngine() {
-		System.out.println("Engine has started" );
-		engineOn = true;
-		System.out.println(engineOn);
-	}
-	
-	void horsePower(int myCar1, int myCar2) {
-		if (myCar1 > myCar2) {
-			System.out.println("812 Wins");
-		} else {
-			System.out.println("488 Wins");
-		}
-	
-}
-}
-
-class Ferrari extends Cars {
-	String engineSize;
-	int horsePower;
-	String accelaration;
-	int seats;
-	String price;
-	
-	public Ferrari(String Make, String Model, int Wheels, String EngineSize, int HorsePower, String Accelaration, int Seats, String Price, boolean engineOn) {
-		super(Make, Model, Wheels);
-		
 		engineSize = EngineSize;
+		topSpeed = TopSpeed;
 		horsePower = HorsePower;
 		accelaration = Accelaration;
-		seats = Seats;
 		price = Price;
+		
 		
 	}
 	
-	   void startEngine() {
-	        engineOn = true;
-	        System.out.println("Engine is on.");
-	    }
-	    void stopEngine() {
-	        engineOn = false;
-	        System.out.println("Engine is off.");
-	    }
+
+	
+	// Horse Power Tests ----------------------------------------------------------------------------------------------
+	void horsePowerAudi(int A3, int A4) {
+		if (A3 > A4) {
+			
+		} else {
+			System.out.println("A4 Wins");
+		}
+	}
+	
+	
+	void horsePowerBMW(int Series7, int Series8) {
+		if (Series7 > Series8) {
+			System.out.println("7 Series Wins");
+		} else {
+			System.out.println("8 Series Wins");
+		}
+		
+	}
+	
+	
+	void horsePowerMercedes(int EClass, int SClass) {
+		if (EClass > SClass) {
+			System.out.println("E Class Wins");
+		} else {
+			System.out.println("S Class Wins");
+		}
+	}
+
+	
+//	accelaration Tests --------------------------------------------------------------------------------------------------
+	
+	void accelarationMercedes(float EClass, float SClass) {
+		if (EClass < SClass) {
+			System.out.println("E Class Wins in accelaration");
+		} else {
+			System.out.println("S Class Wins in accelaration");
+		}
+	}
+}
+
+
+
+
+class Audi extends Cars {
+
+	public Audi(String Make, String Model, float EngineSize, int TopSpeed, int HorsePower, float Accelaration, int Price) {
+		super(Make, Model, EngineSize, TopSpeed, HorsePower, Accelaration, Price);
+		
+		
+		}
+	
 	    
 	    void tuneEngine() {
 	    	horsePower =+ 10;
-	    	accelaration += "3 Seconds";
+	    	accelaration += 3;
 	    }
 	    
 }
+
+
+class BMW extends Cars {
+
+	public BMW(String Make, String Model, float EngineSize, int TopSpeed, int HorsePower, float Accelaration, int Price) {
+		super(Make, Model, EngineSize, TopSpeed, HorsePower, Accelaration, Price);
+		
+		
+		}
 	
+	    
+	    void tuneEngine() {
+	    	horsePower =+ 10;
+	    	accelaration += 3;
+	    }
+	    
+}
+
+
+class Mercedes extends Cars {
+
+	public Mercedes(String Make, String Model, float EngineSize, int TopSpeed, int HorsePower, float Accelaration, int Price) {
+		super(Make, Model, EngineSize, TopSpeed, HorsePower, Accelaration, Price);
+		
+		
+		}
+	
+	    
+	    void tuneEngine() {
+	    	horsePower =+ 10;
+	    	accelaration += 3;
+	    }
+	    
+}
+
+
--- TopTrumpsChallenge/src/TopTrumpsChallenge.java
+++ TopTrumpsChallenge/src/TopTrumpsChallenge.java
@@ -1,18 +1,43 @@
 
 public class TopTrumpsChallenge {
 		public static void main(String[]args) {
+//			Audi Class --------------------------------------------------------------------------------------------------
 			
-			Ferrari myCar1 = new Ferrari("Ferrari", "812", 4, "V12", 812, "3 Seconds", 2, "£300,000", true);
-			Ferrari myCar2 = new Ferrari("Ferrari", "488", 4, "V8", 530, "3.5 Seconds", 2, "£250,000", true);
+			Audi A3 = new Audi("Audi", "A3", 2f, 130, 140, 6.6f, 24375);
 			
-			System.out.println(myCar1.make);
+			Audi A4 = new Audi("Audi", "A4", 3f, 141, 150, 5.2f, 29505);
 			
-			myCar1.horsePower(myCar1.horsePower, myCar2.horsePower);
+			System.out.println(A3.make);
+			
+			A3.horsePowerAudi(A3.horsePower, A4.horsePower);
 			
 			
+//		BMW Class -------------------------------------------------------------------------------------------------------	
+			BMW Series7 = new BMW("BMW", "7 Series", 3f, 155, 390, 5.7f, 69510);
 			
+			BMW Series8 = new BMW("BMW", "8 Series", 4.4f, 155, 400, 3.8f, 69225);
+			
+			System.out.println(Series7.make);
+			
+			Series7.horsePowerBMW(Series7.horsePower, Series8.horsePower);
+			
+			
+//		Mercedes Class -------------------------------------------------------------------------------------------------------	
 
-		
+			Mercedes EClass = new Mercedes("Mercedes", "EClass", 2f, 156, 385, 5.6f, 69999);
+			
+			Mercedes SClass = new Mercedes("Mercedes", "SClass", 3f, 159, 399, 3.9f, 71000);
+			
+			System.out.println(EClass.make);
+			
+			EClass.horsePowerMercedes(EClass.horsePower, SClass.horsePower);
+			
+			EClass.accelarationMercedes(EClass.accelaration, SClass.accelaration);
+			
+			System.out.println();
+			
+			
 	}
 
 }
+
